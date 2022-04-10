e4390a4e355609c741d2fa7515494d238f8ae7cf
Richard Duffy <rduffy@slack-corp.com>
2022-04-10 21:24:04 +0100

    Updated files for owasp zap

--- routes/math.js
+++ routes/math.js
@@ -1,61 +1,41 @@
+var createError = require('http-errors');
 var express = require('express');
-var router = express.Router();
-var utils = require('../modules/utils')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect("/math/add");
+var indexRouter = require('./routes/index');
+var mathRouter = require('./routes/math');
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/math', mathRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-/* GET home page. */
-router.get('/add', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
-      result = utils.add(parseFloat(num1), parseFloat(num2))
-  }
-  res.render('compute', { title: 'add' ,result:result, num1, num2, symbol:"+" });
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-router.get('/divide', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
-    try {
-      result = utils.divide(parseFloat(num1), parseFloat(num2))
-    } catch (e) {
-      result = "error"
-    }
-      
-  }
-  res.render('compute', { title: 'divide' ,result:result, num1, num2, symbol:"/" });
-
-});
-
-router.get('/multiply', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
-      result = utils.multiply(parseFloat(num1), parseFloat(num2))
-  }
-  res.render('compute', { title: 'multiply' ,result:result, num1, num2, symbol:"*" });
-
-});
-
-
-router.get('/subtract', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  console.log(req.query)
-  if(num1 !=='' && num2 !=='') {
-      result = utils.subtract(parseFloat(num1), parseFloat(num2))
-  }
-  res.render('compute', { title: 'subtract' ,result:result, num1, num2, symbol:"-" });
-
-});
-
-
-module.exports = router;
+module.exports = app;
